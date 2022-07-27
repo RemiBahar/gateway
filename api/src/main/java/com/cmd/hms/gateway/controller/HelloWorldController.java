f70d18bf0b847478bbebc0a43a95f2936cc12c88
RemiBahar <remi.bahar.184.rb@gmail.com>
2022-07-27 20:18:01 +0100

    example authorisation gender title

--- api/src/main/java/com/cmd/hms/gateway/controller/HelloWorldController.java
+++ api/src/main/java/com/cmd/hms/gateway/controller/HelloWorldController.java
@@ -1,10 +1,24 @@
 package com.cmd.hms.gateway.controller;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cmd.hms.gateway.model.MyUserDetails;
 @RestController
 public class HelloWorldController {
     @RequestMapping({ "/Patients/*" })
     public String firstPage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String test = authentication.getAuthorities().toString();
+        if(test.contains("USER")){
+            System.out.println("hello" + test);
+        }
+        
+        /*System.out.print(authentication.getAuthorities());
+        boolean hasUserRole = authentication.getAuthorities().stream()
+          .anyMatch(r -> r.getAuthority().equals("USER"));
+          System.out.println(hasUserRole);*/
         return "Hello World";
     }
 }
--- api/src/main/java/com/cmd/hms/gateway/model/Gender.java
+++ api/src/main/java/com/cmd/hms/gateway/model/Gender.java
@@ -1,5 +1,7 @@
 package com.cmd.hms.gateway.model;
 
+import java.lang.reflect.Array;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,12 +12,21 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 @Entity
 @Table(name="gender")
 public class Gender {
     /** Represents a gender. Decoupled from Patient by being unidirectional, allowing it to be reused for different classes.
     */
-
+    /* 
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    
+    boolean isAdmin = authentication.getAuthorities().stream()
+      .anyMatch(r -> r.getAuthority().equals("ADMIN"));*/
+      
     // Fields
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Use Id sequencing unique for this table
@@ -27,6 +38,19 @@ public class Gender {
     @Size(max=100)
     private String Title;
 
+    public Boolean hasRole(String role){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String[] roles = authentication.getAuthorities().toString().split(",", -1);
+        for (int i = 0; i < roles.length; i++) {
+            if(roles[i].contains(role)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    
     // Getters and setters
     public Long getGenderId() {
         return GenderId;
@@ -34,14 +58,20 @@ public class Gender {
 
     public void setGenderId(Long GenderId) {
         this.GenderId = GenderId;
+        
     }
 
     public String getTitle() {
         return Title;
     }
-
+    
+    @PreAuthorize("hasRole('ADMIN')")
     public void setTitle(String Title) {
-        this.Title = Title;
+        if(this.hasRole("USER")){
+            System.out.println("set title");
+            this.Title = Title;
+        }
+        
     }
     
 }
